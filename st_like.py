1454486bfd0674a87a7ab02240eabb038b8bce4b
XDASADX <leemailbox1@gmail.com>
2016-02-10 18:20:59 +0800

    Update st_like.py
    added every photo's code for instantly checking

--- st_like.py
+++ st_like.py
@@ -3,9 +3,11 @@ import requests
 import os
 
 
+code_addr='https://instagram.com/p/'
+
 #os.mkdir(os.getcwd()+'\\data')
 
-id='fun1imit'
+id='natgeo'
 d_f=open(os.getcwd()+'\\data\\'+'stat_'+id+'.csv','w')
 
 
@@ -19,9 +21,12 @@ con=r.content
 start_i=r'"start_cursor":"[0-9]*"'
 end_i=r'end_cursor":"[0-9]*"'
 like=r'"likes":{"count":[0-9]*},"'
+code_i=r'{"code":"[\w\-]+"'
+
 start_cursor=re.findall(start_i,con.decode())
 end_cursor=re.findall(end_i,con.decode())
 like_list=re.findall(like,con.decode())
+code_list=re.findall(code_i,con.decode())
 
 start_var=start_cursor[0]
 start_var=start_var[16:-1]
@@ -30,11 +35,23 @@ end_var=end_cursor[0]
 end_var=end_var[13:-1]
 
 like_list_f=[]
+code_list_f=[]
 
-for i in like_list:
-	i=i[17:-3]
-	like_list_f.append(i)
-	d_f.write(i+'\n')
+print(code_list)
+print(len(code_list))
+
+for i in range(0,len(like_list)):
+	a=like_list[i]
+	a=a[17:-3]
+	like_list_f.append(a)
+	d_f.write(a+',')
+
+	b=code_list[i]
+	b=b[9:-1]
+	code_list_f.append(b)
+	d_f.write(code_addr+b+'\n')
+
+
 
 pagenum=1
 
@@ -47,10 +64,12 @@ while(start_var!=end_var):
 	start_i=r'"start_cursor":"[0-9]*"'
 	end_i=r'end_cursor":"[0-9]*"'
 	like=r'"likes":{"count":[0-9]*},"'
+	code_i=r'{"code":"[\w\-]+"'
 
 	start_cursor=re.findall(start_i,con.decode())
 	end_cursor=re.findall(end_i,con.decode())
 	like_list=re.findall(like,con.decode())
+	code_list=re.findall(code_i,con.decode())
 
 	start_var=start_cursor[0]
 	start_var=start_var[16:-1]
@@ -58,10 +77,18 @@ while(start_var!=end_var):
 	end_var=end_cursor[0]
 	end_var=end_var[13:-1]
 
-	for i in like_list:
-		i=i[17:-3]
-		like_list_f.append(i)
-		d_f.write(i+'\n')
+	
+	for i in range(0,len(like_list)):
+		a=like_list[i]
+		a=a[17:-3]
+		like_list_f.append(a)
+		d_f.write(a+',')
+	
+		b=code_list[i]
+		b=b[9:-1]
+		code_list_f.append(b)
+		d_f.write(code_addr+b+'\n')
+
 
 print (len(like_list_f))
 	
